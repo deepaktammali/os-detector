d87a327d9a1e252e05a4c30a3eb6e34d6cccdb86
Deepak Tammali <deepak.tammali627@gmail.com>
2024-08-29 21:32:14 +0530

    feat: detect and print os

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useMemo } from 'react';
+import './App.css';
+
+const isAndroidDevice = (userAgent) => {
+  if (!userAgent || userAgent.length === 0) {
+    return false;
+  }
+
+  const androidRegex = new RegExp("android", "i");
+  return androidRegex.test(userAgent);
+}
+
+function isIOSDevice(userAgent) {
+  const iosRegex = new RegExp("\\biPhone.*Mobile|\\biPod|\\biPad|AppleCoreMedia", "i")
+  return iosRegex.test(userAgent)
+}
+
+function isIpadDevice(userAgent) {
+  const macRegex = new RegExp("Mac", "i");
+  return macRegex.test(userAgent) && navigator.maxTouchPoints && navigator.maxTouchPoints > 2;
+}
+
+function App() {
+
+  const device = useMemo(() => {
+    const userAgent = navigator.userAgent;
+
+    const isAndroid = isAndroidDevice(userAgent);
+    const isIOS = isIOSDevice(userAgent);
+    const isIpad = isIpadDevice(userAgent);
+
+    return {
+      isAndroid: isAndroid,
+      isIOS: isIOS || isIpad,
+      isIpad: isIpad,
+    }
+  }, [])
+
+  const isUnknown = !device.isAndroid && !device.isIOS;
+
+  return (
+    <div>
+      <div>IsAndroid : {device.isAndroid}</div>
+      <div>IsIOS : {device.isIOS}</div>
+      <div>IsIpad : {device.isIpad}</div>
+      <div>Us Unknown : {isUnknown}</div>
+      <div>{navigator.userAgent}</div>
+    </div>
+  )
+}
+
+export default App
